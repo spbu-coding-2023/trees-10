66aa1ea6f4504238693a9662a19501f3f3d83065
Xamelllion <sid.vp04@gmail.com>
2024-03-18 14:48:40 +0300

    feat: add leftRotate, rightRotate, getNodeGrandParent, getNodeUncle

--- src/main/kotlin/trees/RBTree.kt
+++ src/main/kotlin/trees/RBTree.kt
@@ -37,6 +37,53 @@ class RBTree<K : Comparable<K>, V>: Tree<K, V, RBNode<K, V>>() {
         return node
     }
 
+    private fun leftRotate(node: RBNode<K, V>) {
+        val pivot = node.right
+        pivot?.parent = node.parent
+        if (node.parent != null) {
+            if (node.parent?.left == node)
+                node.parent?.left = pivot
+            else
+                node.parent?.right = pivot
+        }
+
+        node.right = pivot?.left
+        if (pivot?.left != null)
+            pivot.left?.parent = node
+
+        node.parent = pivot
+        pivot?.left = node
+    }
+
+    private fun rightRotate(node: RBNode<K, V>) {
+        val pivot = node.left
+        pivot?.parent = node.parent
+        if (node.parent != null) {
+            if (node.parent?.left == node)
+                node.parent?.left = pivot
+            else
+                node.parent?.right = pivot
+        }
+
+        node.left = pivot?.right
+        if (pivot?.right != null)
+            pivot.right?.parent = node
+
+        node.parent = pivot
+        pivot?.right = node
+    }
+
+    private fun getNodeGrandParent(node: RBNode<K, V>?) =
+        node?.parent?.parent
+
+    private fun getNodeUncle(node: RBNode<K, V>?): RBNode<K, V>? {
+        val grandParent = getNodeGrandParent(node) ?: return null
+        return if (node?.parent == grandParent.left)
+            grandParent.right
+        else
+            grandParent.left
+    }
+
     override fun insert(key: K, value: V) {
         if (root == null) {
             root = RBNode(key, value)
