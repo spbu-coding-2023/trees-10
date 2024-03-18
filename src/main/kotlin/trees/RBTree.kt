177ee6be0bac25ba3797543c7f13fe1c24dfde02
Xamelllion <sid.vp04@gmail.com>
2024-03-18 14:46:27 +0300

    feat: add simple insert and toString methods to RBTree class

--- src/main/kotlin/trees/RBTree.kt
+++ src/main/kotlin/trees/RBTree.kt
@@ -4,6 +4,45 @@ import primitives.Tree
 import nodes.RBNode
 
 class RBTree<K : Comparable<K>, V>: Tree<K, V, RBNode<K, V>>() {
-    override fun insert(key: K, value: V) {}
+    override fun toString() = diagram(root)
+    private fun diagram(node: RBNode<K, V>?,
+                        top: String = "",
+                        root: String = "",
+                        bottom: String = ""): String {
+        return node?.let {
+            if (node.left == null && node.right == null) {
+                "$root${node.key}\n"
+            } else {
+                diagram(node.right, "$top ", "$top┌──", "$top│ ") +
+                        root + "${node.key}\n" + diagram(node.left,
+                    "$bottom│ ", "$bottom└──", "$bottom ")
+            }
+        } ?: "${root}null\n"
+    }
+
+    private fun recInsert(key: K, value: V, node: RBNode<K, V>?): RBNode<K, V>? {
+        node?.let {
+            if (key < node.key) {
+                if (node.left == null) {
+                    node.left = RBNode(key, value)
+                } else  recInsert(key, value, node.left)
+            } else {
+                if (node.right == null) {
+                    node.right = RBNode(key, value)
+                } else {
+                    recInsert(key, value, node.right)
+                }
+            }
+        }
+        return node
+    }
+
+    override fun insert(key: K, value: V) {
+        if (root == null) {
+            root = RBNode(key, value)
+            return
+        }
+        val newNode = recInsert(key, value, root)
+    }
     override fun delete(key: K) {}
 }
