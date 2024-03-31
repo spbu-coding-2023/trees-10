e2f0f3d6c68f5065626918200f3aa3635f422e3d
Avgust Varlamov <46491960+vicebaraban@users.noreply.github.com>
2024-03-31 19:30:24 +0300

    upd imports

--- src/main/kotlin/trees/AVLTree.kt
+++ src/main/kotlin/trees/AVLTree.kt
@@ -1,3 +1,8 @@
+package trees
+
+import primitives.Tree
+import nodes.AVLNode
+
 class AVLTree<K : Comparable<K>, V>: Tree<K, V, AVLNode<K, V>>() {
     override fun insert(key: K, value: V) {
         root = insertRec(root, key, value)
