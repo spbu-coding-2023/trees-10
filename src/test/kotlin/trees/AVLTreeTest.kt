55412f408dd450b12b4a9232dcad01f3f8113874
Avgust Varlamov <46491960+vicebaraban@users.noreply.github.com>
2024-04-03 18:19:27 +0300

    added 3 new tests

--- src/test/kotlin/trees/AVLTreeTest.kt
+++ src/test/kotlin/trees/AVLTreeTest.kt
@@ -6,6 +6,7 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
 class AVLTreeTest {
+
     @Test
     fun `test 1 insertion in empty tree`() {
         val tree = AVLTree<Int, String>()
@@ -35,5 +36,42 @@ class AVLTreeTest {
         tree.insert(2, "B") // right child
         tree.delete(1)
         assertEquals(Pair(2, "B"), Pair(tree.root?.key, tree.root?.value))
+    }
+
+    @Test
+    fun `test 5 insertion and height balance`() {
+        val tree = AVLTree<Int, String>()
+        tree.insert(1, "A")
+        tree.insert(2, "B")
+        tree.insert(3, "C")
+        assertEquals(2, tree.root?.height)
+    }
+
+    @Test
+    fun `test 6 tree balancing`() {
+        val tree = AVLTree<Int, String>()
+        tree.insert(4, "A")
+        tree.insert(2, "B")
+        tree.insert(6, "C")
+        tree.insert(1, "D")
+        tree.insert(3, "E")
+        tree.insert(5, "F")
+        tree.insert(7, "G")
+
+        assertEquals(4, tree.root?.key)
+        assertEquals(2, tree.root?.left?.key)
+        assertEquals(1, tree.root?.left?.left?.key)
+        assertEquals(3, tree.root?.left?.right?.key)
+        assertEquals(6, tree.root?.right?.key)
+        assertEquals(7, tree.root?.right?.right?.key)
+        assertEquals(5, tree.root?.right?.left?.key)
+    }
+
+    @Test
+    fun `test 7 insert duplicate key`() {
+        val tree = AVLTree<Int, String>()
+        tree.insert(1, "A")
+        tree.insert(1, "B")
+        assertEquals("B", tree.search(1))
     }
 }
