6325c55e989f39363cf82644baf419deccff5084
Avgust Varlamov <46491960+vicebaraban@users.noreply.github.com>
2024-04-03 17:57:09 +0300

    fix test 1, 4

--- src/test/kotlin/trees/AVLTreeTest.kt
+++ src/test/kotlin/trees/AVLTreeTest.kt
@@ -7,22 +7,21 @@ import kotlin.test.assertNull
 
 class AVLTreeTest {
     @Test
-    fun `insertion in empty tree`() {
+    fun `test 1 insertion in empty tree`() {
         val tree = AVLTree<Int, String>()
         tree.insert(1, "A")
-        val testNode = AVLNode(1, "A")
-        assertEquals(testNode, tree.root)
+        assertEquals(Pair(1, "A"), Pair(tree.root?.key, tree.root?.value))
     }
 
     @Test
-    fun `insertion and search`() {
+    fun `test 2 insertion and search`() {
         val tree = AVLTree<Int, String>()
         tree.insert(1, "A")
         assertEquals("A", tree.search(1))
     }
 
     @Test
-    fun `insertion and deletion`() {
+    fun `test 3 insertion and deletion`() {
         val tree = AVLTree<Int, String>()
         tree.insert(1, "A")
         tree.delete(1)
@@ -30,12 +29,11 @@ class AVLTreeTest {
     }
 
     @Test
-    fun `root deletion`() {
+    fun `test 4 root deletion`() {
         val tree = AVLTree<Int, String>()
         tree.insert(1, "A") // root
         tree.insert(2, "B") // right child
         tree.delete(1)
-        val newRoot = AVLNode(2, "B")
-        assertEquals(newRoot, tree.root)
+        assertEquals(Pair(2, "B"), Pair(tree.root?.key, tree.root?.value))
     }
 }
