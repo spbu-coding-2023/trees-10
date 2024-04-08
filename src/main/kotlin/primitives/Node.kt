852f4122345c8e4b586a077998e39afdfc224bf9
vicebaraban <varl.avgust@yandex.ru>
2024-04-08 21:42:53 +0300

    upd Node constructor

--- src/main/kotlin/primitives/Node.kt
+++ src/main/kotlin/primitives/Node.kt
@@ -1,8 +1,6 @@
 package primitives
 
-abstract class Node<K : Comparable<K>, V, N : Node<K, V, N>>(k: K, v:V) {
-    var key: K = k
-    var value: V = v
+abstract class Node<K : Comparable<K>, V, N : Node<K, V, N>>(var key: K, var value: V) {
     var right: N? = null
     var left: N? = null
     var parent: N? = null
