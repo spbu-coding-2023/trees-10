16e675e400c5392230a6462ec32b1f9234aadda7
Viacheslav Sidorov <sid.vp04@gmail.com>
2024-04-11 13:52:19 +0300

    feat: move field 'parent' from Node to RBNode

--- src/main/kotlin/nodes/RBNode.kt
+++ src/main/kotlin/nodes/RBNode.kt
@@ -7,4 +7,5 @@ class RBNode<K : Comparable<K>, V>(k: K, v: V) : Node<K, V, RBNode<K, V>>(k, v) 
         RED, BLACK
     }
     var color = Colors.BLACK
+    var parent: RBNode<K, V>? = null
 }
--- src/main/kotlin/primitives/Node.kt
+++ src/main/kotlin/primitives/Node.kt
@@ -3,5 +3,4 @@ package primitives
 abstract class Node<K : Comparable<K>, V, N : Node<K, V, N>>(var key: K, var value: V) {
     var right: N? = null
     var left: N? = null
-    var parent: N? = null
 }
